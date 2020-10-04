2575ca626a10424ffaee9227794d660c9f85f819
Kunlachart <kunlachart.p@ku.th>
2020-10-04 15:44:38 +0700

    atm bankaccount Post service

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -26,5 +26,32 @@ public class BankAccountService {
 
         return Arrays.asList(accounts);
     }
-    // . . . other code . . .
+
+//    public List<BankAccount> getBankAccounts(){
+//        String url ="http://localhost:8091/api/bankaccount";
+//        ResponseEntity<BankAccount[]> response = restTemplate.getForEntity(url,BankAccount[].class);
+//        BankAccount[] accounts = response.getBody();
+//        return Arrays.asList(accounts);
+//    }
+
+   public  void openBankAccount(BankAccount bankAccount){
+        String url = "http://localhost:8091/api/bankaccount";
+        restTemplate.postForObject(url,bankAccount,BankAccount.class);
+   }
+
+    public void openAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/";
+
+        restTemplate.postForObject(url, bankAccount, BankAccount.class);
+    }
+    public List<BankAccount> getBankAccounts() {
+        String url = "http://localhost:8091/api/bankaccount/";
+
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+
+        BankAccount[] accounts = response.getBody();
+        return Arrays.asList(accounts);
+    }
+
 }
